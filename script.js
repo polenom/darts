7b70691fae0d1390d45be809fd04070f9943a6ab
polenom <vitalimitsevich@yandex.by>
2022-07-19 10:48:01 +0300

    feat: test finish

--- script.js
+++ script.js
@@ -96,11 +96,11 @@ function startgame() {
         if (num == 1) {
             gamestr +=  i.name.length > 5?`<div class="target" id="user-${num}">${i.name.slice(0,3)}..</div>`:`<div class="target" id="user-${num}">${i.name}</div>`;
             defvalsum += `<div class="target" id='sum-${num}'>0</div>`;
-            defvalo += `<div class="target" id='pole-1-${num}'>0</div>`;
+            defvalo += `<div class="target" id='pole-1-${num}' >0</div>`;
         } else {
             gamestr +=  i.name.length > 5?`<div id="user-${num}">${i.name.slice(0,3)}..</div>`:`<div id="user-${num}" >${i.name}</div>`;
             defvalsum += `<div id='sum-${num}'>0</div>`;
-            defvalo += `<div id='pole-1-${num}'>0</div>`;
+            defvalo += `<div  id='pole-1-${num}' >0</div>`;
         }
         num++;
     }
@@ -120,8 +120,8 @@ function startgame() {
             </div>
         </div>
         <div class="realplayer" >
-            <div class="row" id="realplayer-place" >
-                <button id="startbutton" class="hove" onclick="finishgame()" > game finish</button>
+            <div class="row" id="finishtbutton" >
+                <button  class="hove" onclick="finishgame()" > game finish</button>
             </div>
         </div>
         `
@@ -130,10 +130,10 @@ function startgame() {
 
 function createround() {
     const poleround = document.getElementById('rounds')
-    let str = `<div class="round" id="round-${round}" >
+    let str = `<div class="round " id="round-${round}" >
                     <div>${round})</div>`;
     for (let i = 1; i <= game.length; i++ ) {
-        str += `<div id='pole-${round}-${i}'>0</div>`;
+        str += `<div сlass="click" id='pole-${round}-${i}' >0</div>`;
     }
     str += '</div>'
     poleround.innerHTML = str + poleround.innerHTML
@@ -153,6 +153,8 @@ function hoduser() {
     roundsum.innerHTML = userinput.value;
     sum.classList.remove('target');
     roundsum.classList.remove('target');
+    roundsum.setAttribute('ondblclick', `changePoint(${round}, ${hod})`);
+    roundsum.classList.add('click');
     usertarget.classList.remove('target');
     userinput.value = ''
     if (hod + 1 > game.length ) {
@@ -169,4 +171,45 @@ function hoduser() {
     sum.classList.add('target')
     roundsum.classList.add('target');
     usertarget.classList.add('target');
+}
+
+function changePoint(r, h) {
+    const getelem = document.getElementById(`pole-${r}-${h}`)
+    getelem.innerHTML = `
+        <input type="text" value="${getelem.innerHTML}" id="inputok-${r}-${h}">
+        <button id="" class="hove" onclick="changePointOK(${r},${h})" > OK</button>
+    `
+    getelem.removeAttribute('ondblclick');
+}
+
+function changePointOK(r,h) {
+    const getelemok = document.getElementById(`inputok-${r}-${h}`).value;
+    if (getelemok == '' || isNaN(+getelemok)) {
+        console.log('miss input')
+    } 
+    const getelem = document.getElementById(`pole-${r}-${h}`);
+    getelem.innerHTML = `${getelemok}`;
+    getelem.setAttribute('ondblclick', `changePoint(${r},${h})`);
+    game[h-1]['round'][r-1] = +getelemok;
+    const getsum = document.getElementById(`sum-${h}`);
+    getsum.innerHTML = game[h-1].sum();
+   
+}
+
+function finishgame(yes=false) {
+    console.log('yes', yes)
+    if (!yes) {
+        console.log(321)
+        const elemfinish = document.getElementById('finishtbutton');
+        elemfinish.innerHTML = `
+        <p>Do you want finish game?</p>
+        <button  class="hove" onclick="finishgame('yes')" > yes</button>
+        <button  class="hove" onclick="finishgame('no')" > no, game</button>
+    `
+    } else if ( yes == 'yes') {
+        location.reload()
+    } else if ( yes == 'no') {
+        const elemfinish = document.getElementById('finishtbutton');
+        elemfinish.innerHTML = '<button  class="hove" onclick="finishgame()" > game finish</button>'
+    }
 }
